d5b9a8a96bdeb55c0634a5740c88b1d559b58ebd
vikas dhiman <vikas dhiman>
2020-07-04 14:18:21 +0530

    assignment

---
+++ assignments/Module-4-assignment/js/script.js
@@ -0,0 +1,17 @@
+(function(window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  for (var i=0 ; i<names.length ; i++) {
+    var firstLetter = (names[i].toLowerCase()).charAt(0);
+  
+    if (firstLetter==='j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})(window);
+
+
+
